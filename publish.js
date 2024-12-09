b2c912f8e151a9a234407b8baf2073a38fa6a70f
molmos <molmosdev@gmail.com>
2024-12-09 16:20:52 +0100

    Add package-lock.json and implement version increment script for publishing

---
+++ publish.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const { exec } = require("child_process");
+const path = require("path");
+const semver = require("semver");
+
+// Path to the package.json of the project
+const packageJsonPath = path.join(__dirname, "package.json");
+
+// Read the package.json file
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+// Increment the version based on the type of release
+const releaseType = process.argv[2] || "patch"; // default to 'patch' if no argument is provided
+packageJson.version = semver.inc(packageJson.version, releaseType);
+
+// Save the package.json file with the new version
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), "utf8");
+
+// Execute npm publish
+exec("npm publish", (err, stdout, stderr) => {
+  if (err) {
+    console.error(`Error executing npm publish: ${stderr}`);
+    process.exit(1);
+  }
+  console.log(stdout);
+});
